e8f1c1fbbc08ad8d5cae5ccee4a2103106283643
vpatel8 <vpatel8@gmu.edu>
2014-02-24 15:36:35 -0500

    HelloWorld.java
    This is the first testing class of Hello World.

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HelloWorld
+{
+  String message;
+  public HelloWorld()
+  {  
+      message = "Hello World!";
+  }
+	public String getM()
+	{
+		  return message;
+	}
+}
