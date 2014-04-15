9cb3cd820eff52ca67e8dcf0611f607e8ddea839
vpatel8 <vpatel8@gmu.edu>
2014-04-15 02:15:33 -0400

    JUnit Fixes

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -1,8 +1,9 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import junit.framework.TestCase;
 
-public class HelloWorldTest {
+public class HelloWorldTest extends TestCase{
 
 /** A test that always fails. **/
 	@Test public void helloTest() {
