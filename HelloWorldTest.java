176243acc8ba39b7b173b2731895772d15d424ea
vpatel8 <vpatel8@gmu.edu>
2014-04-15 21:12:15 -0400

    Changes for running JUnit Test in cmd

--- HelloWorldTest.java
+++ HelloWorldTest.java
@@ -2,10 +2,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import junit.framework.TestCase;
-import helloworld.HelloWorld;
+//import helloworld.HelloWorld;
 
 public class HelloWorldTest extends TestCase{
 
+	public static void main(String args[])
+  	{
+    		org.junit.runner.JUnitCore.main("HelloWorldTest");
+  	}
 /** A test that always fails. **/
 	@Test public void helloTest() {
 		HelloWorld hw = new HelloWorld();
